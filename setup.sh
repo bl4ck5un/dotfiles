cce75a493b9aa4dd89e24ecf59e4b8896cad5f16
Fan Zhang <bl4ck5unxx@gmail.com>
2017-01-17 12:29:35 -0500

    improve setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,28 @@
 #!/bin/bash -e
 
-sudo apt-get install -y cmake zsh vim git ctags
-sudo apt-get install -y build-essential python-dev python3-dev
+DISTRIBUTION_ID=$(lsb_release -i | awk -F ':' '{print $2}' | sed 's/^[ \t]*//g')
+
+case $DISTRIBUTION_ID in
+	openSUSE)
+		echo 'In openSUSE'
+		sudo zypper install cmake zsh vim git ctags python-devel python3-devel
+        sudo zypper install -t pattern devel_basis
+		;;
+	ubuntu)
+		echo 'In ubuntu'
+		sudo apt-get install -y cmake zsh vim git ctags build-essential python-dev python3-dev
+		;;
+
+	*)
+		;;
+esac
 
 DOTFILE_ROOT=`pwd`
 
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+#ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+ZPREZTO_ROOT=${ZDOTDIR:-$HOME}/.zprezto
+[[ -d $ZPREZTO_ROOT ]] && rm -rf $ZPREZTO_ROOT
+git clone --recursive https://github.com/sorin-ionescu/prezto.git $ZPREZTO_ROOT
 
 for rcfile in $DOTFILE_ROOT/prezto/*; do 
     ln -sf "$rcfile" "$HOME/.$(basename $rcfile)"
@@ -15,13 +31,22 @@ done
 cp -f $DOTFILE_ROOT/.zsh-dummy $HOME/.zshrc
 
 ln -sf $(pwd)/.gitconfig ~/.gitconfig
-mkdir ~/.vim
+mkdir -p ~/.vim
 ln -sf $(pwd)/vim/ftplugin ~/.vim
 ln -sf $(pwd)/vim/plugin ~/.vim
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+if [[ -d ~/.vim/bundle/Vundle.vim ]]
+then
+    pushd ~/.vim/bundle/Vundle.vim/
+    git pull
+    popd
+else
+    git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
 ln -sf $(pwd)/.vimrc ~/.vimrc
 vim +PluginInstall +qall
 
 pushd ~/.vim/bundle/YouCompleteMe
 ./install.py --clang-completer
 popd
+
+echo 'Done. The main environment has been setup.'
