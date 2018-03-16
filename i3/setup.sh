29dadf974d6beb1d1538135773ad7acbb91591a9
Fan Zhang <fanz@linux.com>
2018-03-16 14:38:42 -0400

    i3

--- i3/setup.sh
+++ i3/setup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 mkdir -p $HOME/.config/i3
-ln -sf $(pwd)/config $HOME/.config/i3
+ln -sf $(pwd)/config.base $HOME/.config/i3
 ln -sf $(pwd)/i3status.conf $HOME/.config/i3
+ln -sf $(pwd)/themes $HOME/.config/i3
