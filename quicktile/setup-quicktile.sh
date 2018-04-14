d4f8665ee4d718de77473661e82b3ccd377980cf
Fan Zhang <fanz@linux.com>
2018-04-14 11:30:43 -0400

    zsh & quicktile

--- quicktile/setup-quicktile.sh
+++ quicktile/setup-quicktile.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-command -v pip2 || {
+command -v pip2 >/dev/null || {
     echo "Please install pip2 and try again!"
 }
 
+sudo pip2 install https://github.com/ssokolow/quicktile/archive/master.zip
 mkdir -p ~/.config && ln -sf $PWD/quicktile.cfg ~/.config/quicktile.cfg
+
+echo "Done."
