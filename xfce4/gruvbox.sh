ff643418e832aea49cef6858278c10669ba6478e
Fan Zhang <bl4ck5unxx@gmail.com>
2018-05-05 12:22:41 -0400

    gruvbox xfce-terminal

--- xfce4/gruvbox.sh
+++ xfce4/gruvbox.sh
@@ -1,3 +1,8 @@
+DIR="${XDG_DATA_HOME:-$HOME/.local/share}/xfce4/terminal/colorschemes"
 GRUVBOX_TERM=https://raw.githubusercontent.com/mkarmona/dotfiles/master/.config/xfce4/terminal/colorschemes/gruvbox-dark.theme
 
-wget -P $HOME/.xfce4/terminal/colorschemes/ $GRUVBOX_TERM
+echo "Copying .theme files to $dir ..."
+mkdir -p "$DIR"
+
+wget -P $DIR $GRUVBOX_TERM
+echo "done."
