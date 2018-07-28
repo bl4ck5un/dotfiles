8ac2b6ab4425eb318f08fcec28db88121bdc87d8
Fan Zhang <bl4ck5unxx@gmail.com>
2018-07-28 13:12:23 -0700

    ant themes

--- gnome/theme.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir -p ~/.themes/
-
-wget -O /tmp/ant.zip https://github.com/EliverLara/Ant/archive/v1.1.0.zip
-
-pushd ~/.themes
-unzip -o /tmp/ant.zip
-popd
---
+++ gtk/theme/ant.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+mkdir -p ~/.themes/
+
+pushd ~/.themes
+
+wget -O ant.zip https://github.com/EliverLara/Ant/archive/v1.1.0.zip && unzip ant.zip
+wget -O ant.zip https://github.com/EliverLara/Ant-Dracula/archive/v1.1.0.zip && unzip ant.zip
+wget -O ant.zip https://github.com/EliverLara/Ant-Bloody/archive/v1.1.0.zip && unzip ant.zip
