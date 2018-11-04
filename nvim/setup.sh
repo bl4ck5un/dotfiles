bbe03122438bc4cc5fdb5b2d1029ac016a9757ca
Fan Zhang <bl4ck5unxx@gmail.com>
2018-11-03 21:09:09 -0400

    nvim

--- nvim/setup.sh
+++ nvim/setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 command -v apt-get && {
-    sudo apt-get install --yes python-dev python-pip python3-pip python3-dev python3-pip neovim
+    sudo apt-get install --yes python-dev python-pip python3-pip python3-dev python3-pip \
+        python-setuptools python3-setuptools \
+        neovim
 }
 
 pip2 install --user -U neovim
@@ -9,4 +11,11 @@ pip3 install --user -U neovim
 
 mkdir -p ~/.config/nvim
 ln -sf $(pwd)/init.vim ~/.config/nvim/init.vim
-nvim +PlugClean +PlugUpdate +PlugIn
+nvim +PlugClean +PlugUpdate +PlugIn +qall
+
+sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
+sudo update-alternatives --config vi
+sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
+sudo update-alternatives --config vim
+sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
+sudo update-alternatives --config editor
