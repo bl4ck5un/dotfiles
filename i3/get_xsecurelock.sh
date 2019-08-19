0743b7f922fc2982946a46b65af2cf457cf58ebf
Fan Zhang <bl4ck5unxx@gmail.com>
2019-08-19 00:15:58 -0400

    minor

--- i3/get_xsecurelock.sh
+++ i3/get_xsecurelock.sh
@@ -1,8 +1,13 @@
+set -ex
+
+sudo apt install autotools-dev binutils gcc libc6-dev libpam-dev \
+ libx11-dev libxcomposite-dev libxext-dev libxfixes-dev libxft-dev \
+ libxmuu-dev libxrandr-dev libxss-dev make pkg-config x11proto-core-dev
+
+rm -rf xsecurelock
 git clone https://github.com/google/xsecurelock.git
 cd xsecurelock
 sh autogen.sh
 ./configure --with-pam-service-name=common-auth
 make
 sudo make install
-
-rm -rf xsecurelock
