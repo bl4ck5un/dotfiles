65a93cb9dbf8e79e26efbd62e83cb7f493aeef2a
Fan Zhang <bl4ck5unxx@gmail.com>
2019-08-19 00:20:16 -0400

    enable automatic screenlock

--- i3/get_xsecurelock.sh
+++ i3/get_xsecurelock.sh
@@ -2,7 +2,8 @@ set -ex
 
 sudo apt install autotools-dev binutils gcc libc6-dev libpam-dev \
  libx11-dev libxcomposite-dev libxext-dev libxfixes-dev libxft-dev \
- libxmuu-dev libxrandr-dev libxss-dev make pkg-config x11proto-core-dev
+ libxmuu-dev libxrandr-dev libxss-dev make pkg-config x11proto-core-dev \
+ xss-lock
 
 rm -rf xsecurelock
 git clone https://github.com/google/xsecurelock.git
