191fa31f2144ed0733fab6bf7b8210a3a441caf4
Fan Zhang <bl4ck5unxx@gmail.com>
2019-10-03 22:02:33 -0400

    auto reload

--- i3/config.sh
+++ i3/config.sh
@@ -5,6 +5,8 @@ ln -sf $(pwd)/config.base $HOME/.config/i3
 ln -sf $(pwd)/i3status.conf $HOME/.config/i3
 ln -sf $(pwd)/themes $HOME/.config/i3
 
+test -f ./j4-make-config && ./j4-make-config -r gruvbox
+
 echo "Done."
 echo "Please review config.base and tweak"
 echo "then refer to README"
