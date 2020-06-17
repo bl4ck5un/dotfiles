91c3cf784a74e3edcc4767e76a36da4431f2aad3
Fan Zhang <bl4ck5unxx@gmail.com>
2020-06-16 21:51:51 -0400

    revamp i3 config

--- i3/config.sh
+++ i3/config.sh
@@ -5,13 +5,16 @@ ln -sf $(pwd)/config.base $HOME/.config/i3
 ln -sf $(pwd)/i3status.conf $HOME/.config/i3
 ln -sf $(pwd)/themes $HOME/.config/i3
 
-echo "Installing Ubuntu packages"
+echo "Installing Ubuntu deps packages"
 sudo apt-get install --yes \
     j4-dmenu-desktop \
     fonts-roboto \
     fonts-firacode \
     fonts-font-awesome \
-    xfonts-terminus
+    xfonts-terminus \
+    rofi \
+    pasystray \
+    brightnessctl
 
 test -f ./j4-make-config && ./j4-make-config -r gruvbox
 
