122ceea74d5c6051763a5190266e298ca22e8d4b
Fan Zhang <bl4ck5unxx@gmail.com>
2020-07-19 14:42:40 -0400

    add xob

---
+++ i3/get_xob.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+
+REPO=https://github.com/florentc/xob.git
+BUILD_DEPS="libx11-dev libconfig-dev"
+
+tmpdir=$(mktemp -d /tmp/xob.XXXXXX)
+
+pushd $tmpdir
+sudo apt-get install --yes -qq $BUILD_DEPS
+git clone https://github.com/florentc/xob.git
+cd xob
+make && sudo make install
+sudo apt-get autoremove --purge $BUILD_DEPS
+popd
+
