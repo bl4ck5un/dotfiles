3cbbee611f35a6bd078306835000512184e160c3
Fan Zhang <bl4ck5unxx@gmail.com>
2020-07-19 15:03:12 -0400

    switch to Makefile for installation

--- i3/X/turnon-laptop-screen-if-last/install.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-sudo pip3 install autorandr
-
-sudo ln -sf $PWD/99-turnon-laptop-screen-if-last.rules /etc/udev/rules.d
-ln -sf $PWD/turnon-laptop-screen-if-last.sh $HOME/.local/bin
