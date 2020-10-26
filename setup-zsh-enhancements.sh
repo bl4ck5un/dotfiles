17daa9a9f15f81b592dde8bdd010c024d711ed63
Fan Zhang <bl4ck5unxx@gmail.com>
2020-10-26 12:10:33 -0400

    update

--- setup-zsh-enhancements.sh
+++ setup-zsh-enhancements.sh
@@ -6,5 +6,10 @@ set -euo pipefail
 # direnv
 sudo apt update && sudo apt install direnv
 
+
 # jump https://github.com/gsamokovarov/jump
-go get github.com/gsamokovarov/jump
+if command -v go > /dev/null; then
+    get github.com/gsamokovarov/jump
+else
+    wget https://github.com/gsamokovarov/jump/releases/download/v0.30.1/jump_0.30.1_amd64.deb && sudo dpkg -i jump_0.30.1_amd64.deb
+fi
