fdd4013aa7c9bc71aa483538ede45431fa7c1266
Fan Zhang <bl4ck5unxx@gmail.com>
2025-02-05 19:22:20 -0500

    minor updates

--- setup-zsh-enhancements.sh
+++ setup-zsh-enhancements.sh
@@ -6,17 +6,16 @@ set -euo pipefail
 # direnv
 if command -v apt-get > /dev/null; then
     sudo apt update && sudo apt install direnv
+    wget https://github.com/gsamokovarov/jump/releases/download/v0.30.1/jump_0.30.1_amd64.deb && sudo dpkg -i jump_0.30.1_amd64.deb
 fi
 
 
 if command -v brew > /dev/null; then
-    brew install direnv
+    brew install direnv wget
 fi
 
 
 # jump https://github.com/gsamokovarov/jump
 if command -v go > /dev/null; then
     go install github.com/gsamokovarov/jump@latest
-else
-    wget https://github.com/gsamokovarov/jump/releases/download/v0.30.1/jump_0.30.1_amd64.deb && sudo dpkg -i jump_0.30.1_amd64.deb
 fi
