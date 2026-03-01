4685145db71608e2850388b76c1cec201fbfc463
Fan Zhang <bl4ck5unxx@gmail.com>
2026-03-01 11:26:58 -0500

    aerospace

---
+++ mac/.config/sketchybar/plugins/aerospace.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# make sure it's executable with:
+# chmod +x ~/.config/sketchybar/plugins/aerospace.sh
+
+if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
+    /opt/homebrew/bin/sketchybar --set $NAME background.drawing=on
+else
+    /opt/homebrew/bin/sketchybar --set $NAME background.drawing=off
+fi
